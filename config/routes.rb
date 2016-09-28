c733e57f517068a7feebe863eaf8e6e5365cbb5d
wangxiaoshuai <skywangxiaoshuai@163.com>
2016-09-28 13:10:11 +0800

    部署更新测试

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :students
+  resources :tests
+  resources :users
   root 'static_page#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
